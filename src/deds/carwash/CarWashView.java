e1f634d24abb1103a376d49b33ca1ef36b6a5883
Samuel Nilsson <samuel02@gmail.com>
2012-02-29 14:11:57 +0100

    Added view files

---
+++ src/deds/carwash/CarWashView.java
@@ -0,0 +1,22 @@
+package deds.carwash;
+
+import deds.view.SimView;
+import java.util.Observable;
+
+/**
+ * CarWashView
+ * 
+ * @author samuelnilsson
+ *
+ */
+public class CarWashView extends SimView{
+	
+	public CarWashView() {
+		super();
+		
+	}
+	
+	public void update(Observable arg0, Object arg1) {
+		
+	}
+}
---
+++ src/deds/view/SimView.java
@@ -0,0 +1,23 @@
+package deds.view;
+
+import java.util.Observer;
+import java.util.Observable;
+
+/**
+ * Abstract class for view.
+ * 
+ * @author samuelnilsson
+ *
+ */
+public abstract class SimView implements Observer{
+	
+	public SimView() {
+		
+	}
+	
+	
+	public void update(Observable arg0, Object arg1) {
+		
+	}
+
+}
