e8f86cf8df376aa06581744e8caa8f410feb0815
Sebastian Viklund <sebvik-0@student.ltu.se>
2012-02-29 16:21:02 +0100

    A prototyp of the main function

--- src/SimMain.java
+++ src/SimMain.java
@@ -5,8 +5,15 @@ public class SimMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		carwash.State state = new carwash.State();
+		state.addView( new carwash.View() );
+		
+		deds.Simulator simulator = new deds.Simulator();
+		simulator.setState( state );
+		simulator.addEvent( new carwash.event.Start( 0.0f ) );
+		simulator.addEvent( new deds.event.Stop( 15.0f ) );
+		simulator.run();
 	}
 
 }
