f6305d3890d58c49533966f2bce63dea48311b9b
Samuel Nilsson <samuel02@gmail.com>
2012-03-06 11:42:15 +0100

    Added some commenting/javadoc to random streams

--- src/carwash/randomstreams/ExponentialRandomStream.java
+++ src/carwash/randomstreams/ExponentialRandomStream.java
@@ -1,8 +1,19 @@
-
 package carwash.randomstreams;
 
 import java.util.Random;
 
+/**
+ * Dynamic object that creates an exponential random stream.
+ * 
+ * The constructor takes a lambda and a seed to generate
+ * the stream.
+ * 
+ * For more info on exponential distribution see
+ * http://en.wikipedia.org/wiki/Exponential_distribution
+ * 
+ * @author samuelnilsson
+ *
+ */
 public class ExponentialRandomStream {
 	
 	private Random rand;
--- src/carwash/randomstreams/UniformRandomStream.java
+++ src/carwash/randomstreams/UniformRandomStream.java
@@ -1,9 +1,19 @@
-
 package carwash.randomstreams;
 
 import java.util.Random;
 
-
+/**
+ * Dynamic object that creates a uniform random stream of numbers.
+ * 
+ * The constructor takes lower and upper bound as well as
+ * a seed to generate the stream.
+ * 
+ * For more info on uniform distribution see 
+ * http://en.wikipedia.org/wiki/Uniform_distribution_%28continuous%29#Sampling_from_a_uniform_distribution
+ * 
+ * @author samuelnilsson
+ *
+ */
 public class UniformRandomStream {
 
 	private Random rand;
