2bc0dba755eef8988f07877c55ccd0622b52a86a
Samuel Nilsson <samuel02@gmail.com>
2012-03-07 09:54:17 +0100

    Generated new javadoc, new formatting on CarWashState

--- src/carwash/CarWashState.java
+++ src/carwash/CarWashState.java
@@ -1,6 +1,5 @@
 package carwash;
 
-
 import java.util.Vector;
 
 import carwash.randomstreams.*;
@@ -8,85 +7,83 @@ import carwash.randomstreams.*;
 import deds.*;
 import deds.events.Event;
 
-
 /**
  * The state of a car wash for a car wash simulation.
  * 
  * @author Sebastian
  * 
  */
-public class CarWashState extends State
-{
-	private final int   DEFAULT_SEED              = 1234;
-	
-	private final int   DEFAULT_NUM_FAST_MACHINES = 2;
-	private final float DEFAULT_FAST_DISTR_MIN    = 2.8f;
-	private final float DEFAULT_FAST_DISTR_MAX    = 4.6f;
+public class CarWashState extends State {
+	private final int DEFAULT_SEED = 1234;
 
-	private final int   DEFAULT_NUM_SLOW_MACHINES = 2;
-	private final float DEFAULT_SLOW_DISTR_MIN    = 3.5f;
-	private final float DEFAULT_SLOW_DISTR_MAX    = 6.7f;
-	
+	private final int DEFAULT_NUM_FAST_MACHINES = 2;
+	private final float DEFAULT_FAST_DISTR_MIN = 2.8f;
+	private final float DEFAULT_FAST_DISTR_MAX = 4.6f;
+
+	private final int DEFAULT_NUM_SLOW_MACHINES = 2;
+	private final float DEFAULT_SLOW_DISTR_MIN = 3.5f;
+	private final float DEFAULT_SLOW_DISTR_MAX = 6.7f;
+
 	private final float DEFAULT_EXP_DISTRI_LAMBDA = 2.0f;
 
-	private final int   DEFAULT_MAX_QUEUE_SIZE    = 5;
+	private final int DEFAULT_MAX_QUEUE_SIZE = 5;
 
-	private int   seed;
-	
-	private int   nFastMachines;
-	private int   nFastAvalible;
+	private int seed;
+
+	private int nFastMachines;
+	private int nFastAvalible;
 	private float fastDistrMin;
 	private float fastDistrMax;
 	private UniformRandomStream fastRandStream;
 
-	private int   nSlowMachines;
-	private int   nSlowAvalible;
+	private int nSlowMachines;
+	private int nSlowAvalible;
 	private float slowDistrMin;
 	private float slowDistrMax;
 	private UniformRandomStream slowRandStream;
-	
+
 	private float expDistrLambda;
 	private ExponentialRandomStream expRandStream;
-	
-	private int   maxQueueSize;
+
+	private int maxQueueSize;
 	private FIFO<Car> carQueue;
-	
+
 	private float machineIdleTime;
 	private float queueTime;
-	
-	private int   nCars;
-	private int   nRejectedCars;
-	private int   nextCarId;
 
+	private int nCars;
+	private int nRejectedCars;
+	private int nextCarId;
 
-	public CarWashState()
-	{
+	public CarWashState() {
 		seed = DEFAULT_SEED;
-		
-		nFastMachines  = DEFAULT_NUM_FAST_MACHINES;
-		nFastAvalible  = nFastMachines;
-		fastDistrMin   = DEFAULT_FAST_DISTR_MIN;
-		fastDistrMax   = DEFAULT_FAST_DISTR_MAX;
-		fastRandStream = new UniformRandomStream( fastDistrMin, fastDistrMax, seed );
-		
-		nSlowMachines  = DEFAULT_NUM_SLOW_MACHINES;
-		nSlowAvalible  = nSlowMachines;
-		slowDistrMin   = DEFAULT_SLOW_DISTR_MIN;
-		slowDistrMax   = DEFAULT_SLOW_DISTR_MAX;
-		slowRandStream = new UniformRandomStream( slowDistrMin, slowDistrMax, seed );
-		
+
+		nFastMachines = DEFAULT_NUM_FAST_MACHINES;
+		nFastAvalible = nFastMachines;
+		fastDistrMin = DEFAULT_FAST_DISTR_MIN;
+		fastDistrMax = DEFAULT_FAST_DISTR_MAX;
+		fastRandStream = new UniformRandomStream(fastDistrMin, fastDistrMax,
+				seed);
+
+		nSlowMachines = DEFAULT_NUM_SLOW_MACHINES;
+		nSlowAvalible = nSlowMachines;
+		slowDistrMin = DEFAULT_SLOW_DISTR_MIN;
+		slowDistrMax = DEFAULT_SLOW_DISTR_MAX;
+		slowRandStream = new UniformRandomStream(slowDistrMin, slowDistrMax,
+				seed);
+
 		expDistrLambda = DEFAULT_EXP_DISTRI_LAMBDA;
-		expRandStream  = new ExponentialRandomStream( expDistrLambda, seed );
-		
+		expRandStream = new ExponentialRandomStream(expDistrLambda, seed);
+
 		maxQueueSize = DEFAULT_MAX_QUEUE_SIZE;
-		carQueue     = new FIFO<Car>();
-		
+		carQueue = new FIFO<Car>();
+
 		machineIdleTime = 0;
-		queueTime       = 0;
-		
-		nCars         = 0;
+		queueTime = 0;
+
+		nCars = 0;
 		nRejectedCars = 0;
-		nextCarId     = 0;
+		nextCarId = 0;
 	}
 
 	/**
@@ -94,8 +91,7 @@ public class CarWashState extends State
 	 * 
 	 * @return Number of fast washing machines.
 	 */
-	public int getNumFastMachines()
-	{
+	public int getNumFastMachines() {
 		return nFastMachines;
 	}
 
@@ -104,8 +100,7 @@ public class CarWashState extends State
 	 * 
 	 * @return Number of fast washing machines currently available.
 	 */
-	public int getNumFastMachinesAvailable()
-	{
+	public int getNumFastMachinesAvailable() {
 		return nFastAvalible;
 	}
 
@@ -114,8 +109,7 @@ public class CarWashState extends State
 	 * 
 	 * @return Number of slow washing machines.
 	 */
-	public int getNumSlowMachines()
-	{
+	public int getNumSlowMachines() {
 		return nSlowMachines;
 	}
 
@@ -124,8 +118,7 @@ public class CarWashState extends State
 	 * 
 	 * @return Number of slow washing machines currently available.
 	 */
-	public int getNumSlowMachinesAvailable()
-	{
+	public int getNumSlowMachinesAvailable() {
 		return nSlowAvalible;
 	}
 
@@ -134,18 +127,17 @@ public class CarWashState extends State
 	 * 
 	 * @return true if there are one or more machines available (of any kind.)
 	 */
-	public boolean hasVacantMachines()
-	{
+	public boolean hasVacantMachines() {
 		return nFastAvalible + nSlowAvalible > 0;
 	}
 
 	/**
-	 * Gets the minimum and maximum distribution values for fast washing machines.
+	 * Gets the minimum and maximum distribution values for fast washing
+	 * machines.
 	 * 
 	 * @return Array containing min and max distribution values.
 	 */
-	public float[] getFastDistribution()
-	{
+	public float[] getFastDistribution() {
 		float distr[] = new float[2];
 		distr[0] = fastDistrMin;
 		distr[1] = fastDistrMax;
@@ -153,12 +145,12 @@ public class CarWashState extends State
 	}
 
 	/**
-	 * Gets the minimum and maximum distribution values for slow washing machines.
+	 * Gets the minimum and maximum distribution values for slow washing
+	 * machines.
 	 * 
 	 * @return Array containing min and max distribution values.
 	 */
-	public float[] getSlowDistribution()
-	{
+	public float[] getSlowDistribution() {
 		float distr[] = new float[2];
 		distr[0] = slowDistrMin;
 		distr[1] = slowDistrMax;
@@ -170,8 +162,7 @@ public class CarWashState extends State
 	 * 
 	 * @return Exponential distribution lambda value.
 	 */
-	public float getExponentialDistribution()
-	{
+	public float getExponentialDistribution() {
 		return expDistrLambda;
 	}
 
@@ -180,18 +171,16 @@ public class CarWashState extends State
 	 * 
 	 * @return Seed number.
 	 */
-	public int getSeed()
-	{
+	public int getSeed() {
 		return seed;
 	}
-	
+
 	/**
 	 * Generates the time of the next arrival.
 	 * 
 	 * @return Time of the next arrival.
 	 */
-	public float getNewArriveTime()
-	{
+	public float getNewArriveTime() {
 		return getLastEvent().getTime() + (float) expRandStream.next();
 	}
 
@@ -200,8 +189,7 @@ public class CarWashState extends State
 	 * 
 	 * @return Maximum car queue size.
 	 */
-	public int getMaxQueueSize()
-	{
+	public int getMaxQueueSize() {
 		return maxQueueSize;
 	}
 
@@ -210,8 +198,7 @@ public class CarWashState extends State
 	 * 
 	 * @return Current length of the car queue.
 	 */
-	public int getQueueSize()
-	{
+	public int getQueueSize() {
 		return carQueue.size();
 	}
 
@@ -220,8 +207,7 @@ public class CarWashState extends State
 	 * 
 	 * @return true if the car queue is empty.
 	 */
-	public boolean isQueueEmpty()
-	{
+	public boolean isQueueEmpty() {
 		return getQueueSize() == 0;
 	}
 
@@ -230,8 +216,7 @@ public class CarWashState extends State
 	 * 
 	 * @return true if the car queue is full.
 	 */
-	public boolean isQueueFull()
-	{
+	public boolean isQueueFull() {
 		return getQueueSize() == maxQueueSize;
 	}
 
@@ -240,8 +225,7 @@ public class CarWashState extends State
 	 * 
 	 * @return Idle time.
 	 */
-	public float getMachineIdleTime()
-	{
+	public float getMachineIdleTime() {
 		return machineIdleTime;
 	}
 
@@ -250,18 +234,16 @@ public class CarWashState extends State
 	 * 
 	 * @return Queuing time.
 	 */
-	public float getQueueTime()
-	{
+	public float getQueueTime() {
 		return queueTime;
 	}
-	
+
 	/**
 	 * Gets the mean queuing time.
 	 * 
 	 * @return Mean queuing time.
 	 */
-	public float getMeanQueueTime()
-	{
+	public float getMeanQueueTime() {
 		return queueTime / nCars;
 	}
 
@@ -270,8 +252,7 @@ public class CarWashState extends State
 	 * 
 	 * @return Number of cars rejected.
 	 */
-	public int getNumRejectedCars()
-	{
+	public int getNumRejectedCars() {
 		return nRejectedCars;
 	}
 
@@ -280,10 +261,9 @@ public class CarWashState extends State
 	 * 
 	 * @return The new car.
 	 */
-	public Car createNewCar()
-	{
+	public Car createNewCar() {
 		nCars++;
-		return new Car( nextCarId++ );
+		return new Car(nextCarId++);
 	}
 
 	/**
@@ -293,21 +273,20 @@ public class CarWashState extends State
 	 *            The car to be rejected.
 	 * 
 	 * @throws CarNotReadyException
-	 *            if car is in the queue, in a wash, is finished or has been rejected.
+	 *             if car is in the queue, in a wash, is finished or has been
+	 *             rejected.
 	 */
-	public void rejectCar( Car car )
-	{
-		if ( car.getState() != CarState.READY )
-		{
+	public void rejectCar(Car car) {
+		if (car.getState() != CarState.READY) {
 			throw new CarNotReadyException();
 		}
-		
-		car.setState( CarState.REJECTED );
-		
+
+		car.setState(CarState.REJECTED);
+
 		nRejectedCars++;
 		nCars--;
 	}
-	
+
 	/**
 	 * @param car
 	 *            The car to serve.
@@ -315,37 +294,31 @@ public class CarWashState extends State
 	 * @return The time when the wash is done.
 	 * 
 	 * @throws CarNotReadyException
-	 *            if car is in the queue, in a wash, is finished or has been rejected.
+	 *             if car is in the queue, in a wash, is finished or has been
+	 *             rejected.
 	 * 
 	 * @throws NoAvalibleWashingMachinesException
-	 *            if there are no available washing machines.
+	 *             if there are no available washing machines.
 	 */
-	public float serveCar( Car car )
-	{
-		if ( car.getState() != CarState.READY )
-		{
+	public float serveCar(Car car) {
+		if (car.getState() != CarState.READY) {
 			throw new CarNotReadyException();
 		}
-		
+
 		float time = getLastEvent().getTime();
-		
-		if ( nFastAvalible > 0 )
-		{
-			car.setState( CarState.IN_FAST_WASH );
+
+		if (nFastAvalible > 0) {
+			car.setState(CarState.IN_FAST_WASH);
 			nFastAvalible--;
 			time += (float) fastRandStream.next();
-		}
-		else if ( nSlowAvalible > 0 )
-		{
-			car.setState( CarState.IN_SLOW_WASH );
+		} else if (nSlowAvalible > 0) {
+			car.setState(CarState.IN_SLOW_WASH);
 			nSlowAvalible--;
 			time += (float) slowRandStream.next();
-		}
-		else
-		{
+		} else {
 			throw new NoAvalibleWashingMachinesException();
 		}
-		
+
 		return time;
 	}
 
@@ -356,25 +329,23 @@ public class CarWashState extends State
 	 *            The car to place in the queue.
 	 * 
 	 * @throws CarNotReadyException
-	 *            if car is in the queue, in a wash, is finished or has been rejected.
+	 *             if car is in the queue, in a wash, is finished or has been
+	 *             rejected.
 	 * 
 	 * @throws CarQueueFullException
-	 *            if the car queue is full.
+	 *             if the car queue is full.
 	 */
-	public void placeCarInQueue( Car car )
-	{
-		if ( car.getState() != CarState.READY )
-		{
+	public void placeCarInQueue(Car car) {
+		if (car.getState() != CarState.READY) {
 			throw new CarNotReadyException();
 		}
-		
-    	if ( isQueueFull() )
-		{
-    		throw new CarQueueFullException();
+
+		if (isQueueFull()) {
+			throw new CarQueueFullException();
 		}
-    	
-		car.setState( CarState.QUEUED );
-		carQueue.pushBack( car );
+
+		car.setState(CarState.QUEUED);
+		carQueue.pushBack(car);
 	}
 
 	/**
@@ -383,18 +354,16 @@ public class CarWashState extends State
 	 * @return The pulled car.
 	 * 
 	 * @throws CarQueueEmptyException
-	 *            if the car queue is empty.
+	 *             if the car queue is empty.
 	 */
-	public Car pullCarFromQueue()
-	{
-		if ( isQueueEmpty() )
-		{
+	public Car pullCarFromQueue() {
+		if (isQueueEmpty()) {
 			throw new CarQueueEmptyException();
 		}
-		
+
 		Car car = carQueue.popFront();
-		car.setState( CarState.READY );
-		
+		car.setState(CarState.READY);
+
 		return car;
 	}
 
@@ -405,12 +374,10 @@ public class CarWashState extends State
 	 *            The car to remove.
 	 * 
 	 * @throws CarNotInWashException
-	 *            if the car is not in a wash.
+	 *             if the car is not in a wash.
 	 */
-	public void removeCarFromWash( Car car )
-	{
-		switch ( car.getState() )
-		{
+	public void removeCarFromWash(Car car) {
+		switch (car.getState()) {
 		case IN_FAST_WASH:
 			nFastAvalible++;
 			break;
@@ -420,68 +387,52 @@ public class CarWashState extends State
 		default:
 			throw new CarNotInWashException();
 		}
-		
-		car.setState( CarState.FINISHED );
+
+		car.setState(CarState.FINISHED);
 	}
-	
+
 	@Override
-	public void beginEvent( Event event )
-	{
+	public void beginEvent(Event event) {
 		Event lastEvent = getLastEvent();
-		
+
 		float dt;
-		
-		if ( lastEvent == null )
-		{
+
+		if (lastEvent == null) {
 			dt = event.getTime();
-		}
-		else
-		{
+		} else {
 			dt = event.getTime() - lastEvent.getTime();
 		}
-		
-		machineIdleTime += ( nFastAvalible + nSlowAvalible ) * dt;
-		queueTime       += getQueueSize() * dt;
-		
-		super.beginEvent( event );
+
+		machineIdleTime += (nFastAvalible + nSlowAvalible) * dt;
+		queueTime += getQueueSize() * dt;
+
+		super.beginEvent(event);
 	}
-	
-	private enum CarState
-	{
-		READY,
-		REJECTED,
-		QUEUED,
-		IN_FAST_WASH,
-		IN_SLOW_WASH,
-		FINISHED,
+
+	private enum CarState {
+		READY, REJECTED, QUEUED, IN_FAST_WASH, IN_SLOW_WASH, FINISHED,
 	}
-	
-	
+
 	/**
 	 * A class holding information about a car used by the car wash simulation.
 	 * 
 	 * @author Sebastian
-	 *
+	 * 
 	 */
-	public class Car
-	{
+	public class Car {
 		private int id;
 		private CarState state;
 
-
-		private Car( int id )
-		{
+		private Car(int id) {
 			this.id = id;
 			state = CarState.READY;
 		}
-		
-		private CarState getState()
-		{
+
+		private CarState getState() {
 			return state;
 		}
-		
-		private void setState( CarState state )
-		{
+
+		private void setState(CarState state) {
 			this.state = state;
 		}
 
@@ -490,85 +441,76 @@ public class CarWashState extends State
 		 * 
 		 * @return Car id.
 		 */
-		public int getId()
-		{
+		public int getId() {
 			return id;
 		}
 	}
-	
-	
+
 	public class NoAvalibleWashingMachinesException extends RuntimeException {
 
 		private static final long serialVersionUID = 7982650438299570762L;
 	}
-	
+
 	public class CarQueueEmptyException extends RuntimeException {
 
 		private static final long serialVersionUID = -3662676523771613893L;
 	}
-	
+
 	public class CarQueueFullException extends RuntimeException {
 
 		private static final long serialVersionUID = -8352726256107153636L;
 	}
-	
+
 	public class CarNotReadyException extends RuntimeException {
 
 		private static final long serialVersionUID = 5823079140587725283L;
 	}
-	
+
 	public class CarNotInWashException extends RuntimeException {
 
 		private static final long serialVersionUID = -3732622100738630197L;
 	}
 }
 
-
 /**
  * A first in first out queue implementation.
  * 
  * @author Sebastian
- *
+ * 
  * @param <T>
  */
-class FIFO<T>
-{
+class FIFO<T> {
 	private Vector<T> vector;
-	
-	
-	public FIFO()
-	{
+
+	public FIFO() {
 		vector = new Vector<T>();
 	}
-	
+
 	/**
 	 * Gets the size of the queue.
 	 * 
 	 * @return The number of elements in the FIFO.
 	 */
-	public int size()
-	{
+	public int size() {
 		return vector.size();
 	}
-	
+
 	/**
 	 * Adds an item to the back of the queue.
 	 * 
 	 * @param item
-	 *             The item to be added.
+	 *            The item to be added.
 	 */
-	public void pushBack( T item )
-	{
-		vector.add( item );
+	public void pushBack(T item) {
+		vector.add(item);
 	}
-	
+
 	/**
 	 * Removes the item at the front of the queue and returns it.
 	 * 
 	 * @return The item that was removed.
 	 */
-	public T popFront()
-	{
+	public T popFront() {
 		return vector.remove(0);
 	}
 }
